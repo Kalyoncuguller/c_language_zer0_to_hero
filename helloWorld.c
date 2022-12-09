4a157422f91ee9aadee952222464e7a4121339ee
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-09 22:51:28 +0300

    Kalyoncugüller
    Hello World

--- helloWorld.c
+++ helloWorld.c
@@ -1,1 +1,21 @@
 // Hello world hem printf hem de puts ile yaz
+#include <stdio.h>
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
+
+
+
+
+**********************
+#include <stdio.h>
+#include <conio.h>
+int main(){
+char str[25]="Hello World";
+puts(str);
+
+return 0;
+}
+
