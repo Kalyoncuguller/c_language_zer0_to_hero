b3c3a772228d05583aef9e38225d1cd132acda83
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-09 20:33:32 +0300

    Kalyoncugüller
    Ad

--- AdiniYaz.c
+++ AdiniYaz.c
@@ -1,1 +1,10 @@
-// BU programda adinizi yaziniz
+#include <stdio.h>
+
+ int main(){
+	char name[10] = "Güller";
+	char surname[10] = "Kalyoncu";
+
+	printf("Isim : %s \n",name);	
+	printf("Soyisim : %s \n",surname);    
+ }
+
