f41e99a3e7dda3414b675653e02a376d798527fe
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-10 21:46:33 +0300

    Kalyoncugüller
    @themanoftalent

--- friend_name.c
+++ friend_name.c
@@ -3,3 +3,21 @@
 // This is my bff. 
 
 // Good Luck
+#include <stdio.h>
+
+int main()
+{
+   char name[50];
+   
+  printf("Enter the name of the person you like:");
+  scanf("%s",name);
+  
+  if(name == "Güller"){
+      printf("Hurra this is me!");
+  }
+  else{
+      printf("This is my bff");
+  }
+
+    return 0;
+}
