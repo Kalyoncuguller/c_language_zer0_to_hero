5ad016140f2b1f637ddfe1c4153407eca8a1a700
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-12 17:59:28 +0300

    Kalyoncugüller
    @themanoftalent

--- pointer.c
+++ pointer.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
+
 int main()
 {
-  int var = 15;
-  int *ptr =var;
-  printf("Sayi tam olarak : %d\n", var);
+  int var =15; // int bir işaretçi ve değişken bildirimi
+  int *ptr;
+  ptr = &var;    // id değişken adresini ip işaretçisine atar.
 
-  // Notice the use of & before var
-  printf("Sayinin tutuldugu adres : %p", &var);  
+  // Değişken adını kullanarak id değişken değerini ekrana yazar.
+  printf("var değişken değeri: %d\n", var);
+
+  // İşaretçi kullanarak id değişken değerini ekrana yazar.
+  printf("var değişken değeri: %d\n", *ptr);
+
+  // Değişken referansını kullanarak id değişken adresini ekrana yazar.
+  printf("var değişken bellek adresi: %p\n", &var);
+
+  // İşaretçi adını kullanarak id değişken adresini ekrana yazar.
+  printf( "var değişken bellek adresi: %p", ptr);
+
   return 0;
 }
