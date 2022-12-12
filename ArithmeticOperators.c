effbfebfeca9c0d61d74092c0c4b0e26e820134f
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-12 21:24:18 +0300

    Kalyoncugüller
    @themanoftalent

--- ArithmeticOperators.c
+++ ArithmeticOperators.c
@@ -1,23 +1,41 @@
-
-
-// Eksiklikleri gider, hatayi coz. 
-
+//girilen 3 sayının en büyüğünü bulan program
 #include <stdio.h>
+#include <conio.h>
+
+
 int main()
-{
-    int a = 9, 
-        b = 4, 
-        c;
+{ 
+    int a,b,c,enb;
     
-    c = a+b;
-    printf("a+b = %d \n",c);
-    c = a-b;
-    printf("a-b = %d \n",c);
-    c = 
-    printf("a*b = %d \n",c);
-    c = a/b;
-    printf("a/b = %d \n",c);
-    c = 
-    printf("Remainder when a divided by b = %d \n",c);
-    
+    printf("Enter 3 numbers: \n");  
+      scanf("%d %d %d",&a,&b,&c); 
+     
+      if(a>b){
+              
+           if(a>c)
+                  {
+                   enb = a;
+                   }
+                   else
+                   {
+                        enb = c;
+                   }
+           }
+      else 
+           {
+              if(b>c)
+                  {
+                   enb = b;
+                   }
+                   else 
+                   {
+                        enb = c;
+                   }
+         
+           }
+           
+           printf ("Largest number entered %d", enb);
+           
     return 0;
+    
+}
