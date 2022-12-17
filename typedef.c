69aa53d9e6f2852c8028be383a88742cb6c4a985
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-17 23:32:50 +0300

    Kalyoncugüller
    @themanoftalent

--- typedef.c
+++ typedef.c
@@ -1,6 +1,5 @@
 
 // typedef with array
-
 #include <stdio.h>
 
 typedef int Arr[5];
@@ -8,10 +7,10 @@ typedef int Arr[5];
 
 int main()
 {
-Arr temp = {10, 20, 30, 40,50};
+Arr temp = {10, 20, 30, 40, 50};
 printf("typedef using an array\n");
 	
-for (int i = 0; i < 4; i++)
+for (int i = 0; i < 5; i++)
 {
 	printf("%d ", temp[i]);
 }
