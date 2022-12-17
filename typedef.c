450e77fc8b2402c461ee3d141684b938321baef0
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-17 22:24:48 +0300

    Create typedef.c

---
+++ typedef.c
@@ -0,0 +1,19 @@
+
+// typedef with array
+
+#include <stdio.h>
+
+typedef int Arr[5];
+
+
+int main()
+{
+Arr temp = {10, 20, 30, 40,50};
+printf("typedef using an array\n");
+	
+for (int i = 0; i < 4; i++)
+{
+	printf("%d ", temp[i]);
+}
+return 0;
+}
