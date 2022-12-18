c6573c3738481c5df14b262e0d8fde6246a5e3a7
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-18 12:52:09 +0300

    Kalyoncugüller
    @themanoftalent

--- enum4.c
+++ enum4.c
@@ -11,3 +11,51 @@ int main() {
     printf("%d", ay);
     return 0;
 }
+
+//enum and switch-case
+#include <stdio.h>
+
+enum months {January = 1,February,March,April,May,June,July,August,September,October,November,December} i;
+
+int main(){
+  int option;
+
+  printf("Enter a number[1-12]: ");
+  scanf("%d", &option);
+
+  i = option;
+
+  switch (i) {
+    case January: printf("January");
+       break;
+    case February: printf("February");
+       break;
+    case March: printf("March");
+       break;
+    case April: printf("April");
+       break;
+    case May: printf("May");
+       break;
+    case June: printf("June");
+       break;
+    case July: printf("July");
+       break;
+    case August: printf("August");
+       break;
+    case September: printf("September");
+       break;
+    case October: printf("October");
+       break;
+    case November: printf("November");
+       break;
+    case December: printf("December");
+       break;
+    default:
+        printf("Please enter numbers between 1 and 12");
+  }
+  
+  return 0;
+}
+//output:
+//Enter a number[1-12]: 4
+//April
