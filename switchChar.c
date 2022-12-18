d9a2751be861f225c776fa85f37db267a021d067
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-18 12:43:51 +0300

    Kalyoncugüller
    @themanoftalent

--- switchChar.c
+++ switchChar.c
@@ -33,5 +33,47 @@ switch (day) {
 }
       return 0;
 }
+
+
+
+//with enum
+
+#include <stdio.h>
+
+enum days { Monday, Tuesday, Wednesday, Thursday,Friday,Saturday,Sunday } i;
+
+int main(){
+  int option;
+
+  printf("Enter a number[0-6]: ");
+  scanf("%d", &option);
+
+  i = option;
+
+  switch (i) {
+    case Monday: printf("Monday");
+       break;
+    case Tuesday: printf("Tuesday");
+       break;
+    case Wednesday: printf("Wednesday");
+       break;
+    case Thursday: printf("Thursday");
+       break;
+    case Friday: printf("Friday");
+       break;
+    case Saturday: printf("Saturday");
+       break;
+    case Sunday: printf("Sunday");
+       break;
+    default:
+        printf("Please enter numbers between 0 and 6");
+  }
+  
+  return 0;
+}
+
+//output:
+//Enter a number[0-6]: 4
+//Friday
     
   
