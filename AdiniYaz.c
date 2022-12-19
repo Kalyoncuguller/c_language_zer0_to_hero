74371fc349996eb46d8ff974ce17b569ac118b98
HaticeSerin <119755031+HaticeSerin@users.noreply.github.com>
2022-12-19 19:45:53 +0300

    Hatice Serin
    @themanoftalent

--- AdiniYaz.c
+++ AdiniYaz.c
@@ -1,9 +1,19 @@
 // BU programda adinizi yaziniz
-
-#include <stdio.h>
-#include "stdlib.h"
-
+#include "stdio.h"
+#include "string.h"
 int main(){
-    printf("Melisa");
+	int age,age1=21;
+    char name1[15]="hatice";
+    char name[15];
+    printf("Enter your name and age:\n");
+    scanf("%s %d",name,&age);
+    if(strcmp(name,name1)==0&&age==age1){
+    	printf("Ohh,you are good person!");
+	}
+	else{
+		printf("sitdown 0!");
+	}
+    
     return 0;
+
 }
