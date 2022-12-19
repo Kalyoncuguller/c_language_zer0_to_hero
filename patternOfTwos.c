44f04f1b09193e8c6c5ddb17e6dfd89a050e20d8
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2022-12-19 11:49:54 +0300

    Guller

---
+++ patternOfTwos.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int array[8] = {2,3,4,5,6,7,8,9};
+
+    for(int i = 0;i<2;i++){
+       printf("%d ",array[0]);
+    }
+    printf("\n");
+
+    for(int i = 0;i<4;i++){
+       printf("%d ",array[1]);
+    }
+    printf("\n");
+    for(int i = 0;i<1;i++){
+       printf("%d ",array[2]);
+    }
+    printf("\n");
+    for(int i = 0;i<3;i++){
+       printf("%d ",array[3]);
+    }
+    printf("\n");
+    for(int i = 0;i<5;i++){
+       printf("%d ",array[4]);
+    }
+    printf("\n");
+    for(int i = 0;i<2;i++){
+       printf("%d ",array[5]);
+    }
+    printf("\n");
+    for(int i = 0;i<4;i++){
+       printf("%d ",array[6]);
+    }
+    printf("\n");
+    for(int i = 0;i<6;i++){
+       printf("%d ",array[7]);
+    }
+return 0;
+}
