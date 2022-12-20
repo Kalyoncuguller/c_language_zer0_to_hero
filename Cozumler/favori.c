82213a691d2e1719885acc7a173d9340b664261a
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-20 22:03:22 +0300

    Kalyoncugüller
    @themanoftalent

--- Cozumler/favori.c
+++ Cozumler/favori.c
@@ -1,13 +1,18 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    int sayi;
-    printf("favori sayinizi giriniz\n");
-    scanf("%d",&sayi);
-    printf("favori sayiniz %d.",sayi);
+    char dream[100];
+    printf("Enter your dreams:\n");
+    scanf("%[^\n]s",dream);
+    
+    int len = strlen(dream);
+    if(strlen(dream)==0){
+        printf("I can't believe it, your situation is so sad.");
+    }
+    else{
+        printf("Never give up on your dreams");
+    }
     return 0 ;
-
-
-
 }
