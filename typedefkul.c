4a8561266f24e45d428c54b377d5ab98a6f2f69e
MelisaOnal <119419249+MelisaOnal@users.noreply.github.com>
2022-12-20 09:29:02 +0300

    melisa typedef

---
+++ typedefkul.c
@@ -0,0 +1,15 @@
+//typedef 
+
+#include <stdio.h>
+#include <math.h>
+
+typedef int sayi;
+typedef float ondalikli;
+
+int main() {
+    sayi birsayi = 5;
+    ondalikli ondaliklisayi = 5.5;
+    printf("a = %d \n", birsayi);
+    printf("b = %f \n", ondaliklisayi);
+    return 0;
+}
