0d4bf7d89ab469352b6d5e94a98ec33a48f7dc42
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-21 18:47:14 +0300

    Kalyoncugüller
    @themanoftalent

--- sWitchDayCase.c
+++ sWitchDayCase.c
@@ -28,3 +28,39 @@ int main()
      }
     return 0;
 }
+
+//enum ve switch-case ile günler
+
+#include <stdio.h>
+
+enum days { Monday, Tuesday, Wednesday, Thursday,Friday,Saturday,Sunday }  i;
+
+int main(){
+  int option;
+
+  printf("Enter a number[0-6]: ");
+  scanf("%d", &option);
+
+  i = option;
+
+  switch (i) {
+    case Monday: printf("Monday");
+       break;
+    case Tuesday: printf("Tuesday");
+       break;
+    case Wednesday: printf("Wednesday");
+       break;
+    case Thursday: printf("Thursday");
+       break;
+    case Friday: printf("Friday");
+       break;
+    case Saturday: printf("Saturday");
+       break;
+    case Sunday: printf("Sunday");
+       break;
+    default:
+        printf("Please enter numbers between 0 and 6");
+  }
+  
+  return 0;
+}
