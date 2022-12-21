cf9956077586d59245462f3757ff52620a71deae
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-21 18:37:50 +0300

    Kalyoncugüller
    @themanoftalent

--- Union/union2.c
+++ Union/union2.c
@@ -1,24 +1,22 @@
-// union
-
 #include <stdio.h>
 #include <stdlib.h>
 
-union data {
-    int sayi;
-    float ondalik;
-    char yazi[20];
+union information {
+    int age;
+    float grade_av;
+    char name[20];
 };
 
 int main() {
-    union data veri;
-    printf("Enter an integer: ");
-    scanf("%d", &veri.sayi);
-    printf("Sayi: %d \n", veri.sayi);
-    printf("Enter a float: ");
-    scanf("%f", &veri.ondalik);
-    printf("Ondalik: %f \n", veri.ondalik);
-    printf("Enter a string: ");
-    scanf("%s", veri.yazi);
-    printf("Yazi: %s \n", veri.yazi);
+    union information personal;
+    printf("Enter your age: ");
+    scanf("%d", &personal.age);
+    printf("Age: %d \n", personal.age);
+    printf("Enter your grade avarage: ");
+    scanf("%f", &personal.grade_av);
+    printf("Grade avarage: %.2f \n", personal.grade_av);
+    printf("Enter your name: ");
+    scanf("%s", personal.name);
+    printf("Name: %s \n", personal.name);
     return 0;
 }
