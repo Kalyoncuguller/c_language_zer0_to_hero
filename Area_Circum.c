d0087b4d2a05b843137b443c0d2ca346bdc8e951
Kalyoncuguller <118815433+Kalyoncuguller@users.noreply.github.com>
2022-12-21 18:41:45 +0300

    Kalyoncugüller
    @themanoftalent

--- Area_Circum.c
+++ Area_Circum.c
@@ -1,27 +1,46 @@
-
-//Hocam google'dan biraz yardım aldım
-
+//Finding the Area and Circumference of a Circle Using  Function
 #include <stdio.h>
-#include <math.h>
+#define pi 3.1415
 
-#define PI 3.14
-
-void alanbul(float r, float *area) {
-    *area = PI * r * r;
+float area(float radius){
+     float result;
+     result = pi * radius * radius;
+     return  result;
+}
+float circum(float radius){
+     float result;
+     result = 2 * pi * radius;
+     return  result;
 }
 
-void cevrebul(float r, float *circum) {
-    *circum = 2 * PI * r;
+int main(){
+     int circleRadius,choice;
+
+     printf("Enter the radius of the circle: ");
+     scanf("%d",&circleRadius);
+
+     printf("To find the area of 1 circle\nTo find the circumference of 2 circles\nYour choice: ");
+     scanf("%d",&choice);
+
+     if(choice == 1)
+          printf("Area of the circle: %f \n",area(circleRadius));
+     else if(choice == 2) 
+          printf("Circumference of the circle: %f", circum(circleRadius));
+     else
+          printf("You made the wrong choice!");
+    return 0;      
 }
 
+//output:
+//Enter the radius of the circle: 5
+//To find the area of 1 circle
+//To find the circumference of 2 circles
+//Your choice: 1
+//Area of the circle: 78.537498 
 
-int main() {
-    float r, area, circum;
-    printf("çemberin yarıçapını girin: ");
-    scanf("%f", &r);
-    alanbul(r, &area);
-    cevrebul(r, &circum);
-    printf("dairenin alanı = %f \n", area);
-    printf("dairenin çevresi = %f \n", circum);
-    return 0;
-}
+//output:
+//Enter the radius of the circle: 5
+//To find the area of 1 circle
+//To find the circumference of 2 circles
+//Your choice: 2
+//Circumference of the circle: 31.415001
